302df9228f2cf718d8c1b941086239551b81add2
abdelhalim <rioman19@gmail.com>
2024-01-05 10:59:22 -0500

    add status controls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import { Notification } from '$lib/notification';
+
+	let activeIndex = 0;
+
+	const allStatus = ['bridging', 'video', 'complete'];
+	$: activeStatus = allStatus[activeIndex];
+
+	function onClick() {
+		activeIndex = (activeIndex + 1) % allStatus.length;
+	}
 </script>
 
-<div class="p-10">
-	<Notification status="video" name="ryeshrimp" address="0x0000" />
+<div class="p-10 flex flex-col gap-10">
+	<Notification status={activeStatus} name="ryeshrimp" address="0x0000" />
+
+	<button class="bg-neutral-200 px-6 py-2" on:click={onClick}>Change status</button>
 </div>
